55279a71bf13aabec34ca0416d65e3bee866486b
AHSANUL-KARIM <48880695+AHSANUL-KARIM@users.noreply.github.com>
2023-03-11 05:03:14 +0600

    3 files modified

--- app/tab-group-dynamic-example.ts
+++ app/tab-group-dynamic-example.ts
@@ -1,5 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { MatTab } from '@angular/material';
+import { MatTabGroup } from '@angular/material/tabs';
 import { MatTabChangeEvent } from '@angular/material/typings/esm5/tabs/public-api';
 import { DragScrollComponent } from 'ngx-drag-scroll';
 
@@ -14,6 +16,8 @@ import { DragScrollComponent } from 'ngx-drag-scroll';
 export class TabGroupDynamicExample {
   @ViewChild('nav', { read: DragScrollComponent })
   ds: DragScrollComponent;
+  @ViewChild('mtg')
+  mtg: MatTabGroup;
   tabs = ['First', 'Second', 'Third'];
   selected = new FormControl(0);
 
@@ -26,8 +30,9 @@ export class TabGroupDynamicExample {
   }
 
   tabChangeEVent(event: MatTabChangeEvent) {
-    this.selected.setValue(event.index);
-    this.ds.moveTo(event.index);
+    //this.selected.setValue(event.index);
+    console.log(this.mtg);
+    //this.mtg._focusChanged(event.index);
   }
   removeTab(index: number) {
     this.tabs.splice(index, 1);
